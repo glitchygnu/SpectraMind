8f463d8f21c714c9cfd12833a793a75cd7832586
glitchygnu <adrian.berndt@gmx.net>
2025-05-02 20:08:46 +0200

    Update index.php

--- spectramind/index.php
+++ spectramind/index.php
@@ -1,1 +1,13 @@
+<?php
+require_once 'includes/config.php';
 
+$auth = new Auth();
+
+if($auth->isLoggedIn()) {
+    header('Location: dashboard.php');
+    exit;
+} else {
+    header('Location: login.php');
+    exit;
+}
+?>
